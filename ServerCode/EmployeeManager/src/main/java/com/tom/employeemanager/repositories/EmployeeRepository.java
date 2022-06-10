41c8e6abbc71b5608ae61321c458ca255eb05033
Tom Perez <ttomrp@gmail.com>
2022-06-09 21:46:24 -0500

    commenting and TODO updates

--- ServerCode/EmployeeManager/src/main/java/com/tom/employeemanager/repositories/EmployeeRepository.java
+++ ServerCode/EmployeeManager/src/main/java/com/tom/employeemanager/repositories/EmployeeRepository.java
@@ -9,12 +9,6 @@ import com.tom.employeemanager.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    // TODO: unsure why but for now this method causes a 500 Error HTTP response
-    // when trying to delete user by Id. For now just use .deleteById in
-    // EmployeeService
-    // @Query("DELETE e FROM employeemanager e WHERE e.id = ?1")
-    void deleteEmployeeById(Long id);
-
     // @Query("SELECT e FROM employeemanager e WHERE e.id = ?1")
     Optional<Employee> findEmployeeById(Long id);
 }
